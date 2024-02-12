e0e07dff5780fb9cc7806d0a2ee57021459be765
Agustina Chiaravalli <agustinachiaravalli@gmail.com>
2024-02-12 20:52:07 -0300

    cambios sitio M5U4

--- Empiria React/back/app.js
+++ Empiria React/back/app.js
@@ -5,11 +5,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require ('dotenv').config();
-var pool= require('./models/base')
+var pool= require('./models/base');
+var session= require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login');
+var adminRouter = require('./routes/admin/blog');
 
 var app = express();
 
@@ -23,11 +25,32 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'edkednkjfsksetyhbdgtn',
+  cookie: {maxAge: null},
+  resave: false,
+  saveUninitialized: true
+}))
+
+secured = async (req,res,next) => {
+  try {
+    console.log (req.session.id_usuario);
+    if (req.session.id_usuario) {
+      next ();
+    } else {
+      res.redirect ('/admin/login');
+    }    
+  } catch (error){
+    console.log (error);
+  }
+}
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/login', loginRouter);
+app.use('/admin/blog',secured, adminRouter);
 
-pool.query('select * from clientes_nuevos').then (function (resultados){
+pool.query('select * from usuarios').then (function (resultados){
   console.log (resultados)
 });
 
