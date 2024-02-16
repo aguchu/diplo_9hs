7fc9e274e5eae4f756fc8c76206b253e0713258b
Agustina Chiaravalli <agustinachiaravalli@gmail.com>
2024-02-16 16:30:49 -0300

    entrega final tp chiaravalli

--- Empiria React/back/routes/api.js
+++ Empiria React/back/routes/api.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const router = express.Router();
-const blogModel = require('../models/blogModel');
-const cloudinary = require('cloudinary').v2;
+var express = require('express');
+var router = express.Router();
+var blogModel = require('../models/blogModel');
+var cloudinary = require('cloudinary').v2;
+var nodemailer= require ('nodemailer');
+
 
 router.get('/blog', async function (req, res, next) {
     let blog = await blogModel.getBlog();
@@ -25,6 +27,31 @@ router.get('/blog', async function (req, res, next) {
         }
     });
     res.json(blog);
+});
+
+router.post ('/contacto', async (req,res) =>{
+    const mail = {
+        to: 'info@empiriacomunicacion.com.ar',
+        subject: 'Contacto web',
+        html: `${req.body.nombre} se contactó a través de la web y quiere más información a este correo: ${req.body.email}
+        <br> Además, se hizo el siguiente comentario: ${req.body.mensaje} <br> Su tel es: ${req.body.telefono}`
+    }
+
+    const transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user:process.env.SMTP_USER,
+            pass:process.env.SMTP_PASS
+        }
+    });
+    
+    await transport.sendMail(mail)
+
+    res.status(201).json ({
+        error: false,
+        message: 'Mensaje Enviado'
+    });
 });
 
 module.exports = router;
--- Empiria React/front/src/pages/ContactoPage.js
+++ Empiria React/front/src/pages/ContactoPage.js
@@ -1,35 +1,71 @@
 import React from "react";
+import { useState } from "react";
+import axios from 'axios';
 import '../styles/ContactoPage.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone, faEnvelope, faMap } from "@fortawesome/free-solid-svg-icons";
 import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const ContactoPage = (props) => {
+
+    const initialForm = {
+        nombre: '',
+        email: '',
+        telefono: '',
+        mensaje: ''
+    }
+
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState('');
+    const [formData, setFormData] = useState(initialForm);
+
+    const handleChange = e => {
+        const {name,value} = e.target;
+        setFormData (oldData=> ({
+            ...oldData,
+            [name]:value
+        }));  
+    }
+
+    const handleSubmit= async e => {
+        e.preventDefault ();
+        setMsg('');
+        setSending (true)
+        const response = await axios.post ('http://localhost:3000/api/contacto', formData);
+        setSending(false);
+        setMsg (response.data.message);
+        if (response.data.error === false){
+            setFormData(initialForm)
+        }
+    }
+
     return (
         <main className="contacto-container">
             <div className="contacto">
                 <h2>Contacto</h2>
-                <form action="" method="" className="formulario">
+                <form action="/contacto" method="post" className="formulario" onSubmit={handleSubmit}>
                     <p>
                         <label htmlFor="nombre">Nombre</label>
-                        <input type="text" name="nombre" required />
+                        <input type="text" name="nombre" required value={formData.nombre} onChange={handleChange} />
                     </p>
                     <p>
                         <label htmlFor="email">Email</label>
-                        <input type="text" name="email" required />
+                        <input type="text" name="email" required value={formData.email} onChange={handleChange}/>
                     </p>
                     <p>
                         <label htmlFor="telefono">Teléfono</label>
-                        <input type="text" name="telefono" />
+                        <input type="text" name="telefono" value={formData.telefono} onChange={handleChange}/>
                     </p>
                     <p>
                         <label htmlFor="mensaje">Mensaje</label>
-                        <textarea name="mensaje" id="" cols="30" rows="10" required></textarea>
+                        <textarea name="mensaje" id="" cols="30" rows="10" required value={formData.mensaje} onChange={handleChange}></textarea>
                     </p>
                     <p className="acciones">
                         <input type="submit" value="Enviar" />
                     </p>
                 </form>
+                {sending ? <p>Enviando...</p>: null}
+                {msg ? <p>{msg}</p>: null}
             </div>
             <div className="datos">
                 <h4>Otras Vías de Comunicación</h4>
