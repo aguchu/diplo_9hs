d39027c6fe54b61de5031b02819909b598e2481c
Agustina Chiaravalli <agustinachiaravalli@gmail.com>
2024-02-16 14:17:07 -0300

    instalar api y que se vea en front

--- Empiria React/front/src/pages/BlogPage.js
+++ Empiria React/front/src/pages/BlogPage.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../styles/BlogPage.css';
+import axios from 'axios';
+import BlogItem from "../componentes/blog/BlogItem";
+
 const BlogPage= (props)=> {
+
+    const [loading,setLoading] = useState (false);
+    const [blog,setBlog] = useState ([]);
+
+    useEffect (() =>{
+        const cargarBlog =async () =>{
+            setLoading(true);
+            const response = await axios.get('http://localhost:3000/api/blog');
+            setBlog(response.data);
+            setLoading(false);
+        };
+        cargarBlog();
+    }, []);
 
     return (
         <section className="holder">
             <h2>Blog</h2>
-            <h3>Titulo</h3>
-            <h4>Subtitulo</h4>
-            <p>cuerpo</p>
+            {loading ?(
+                <p>Cargando...</p>
+            ) : (
+                blog.map (item=> <BlogItem 
+                    key ={item.id}
+                    title={item.titulo} 
+                    subtitle={item.subtitulo} 
+                    imagen={item.imagen} 
+                    body={item.cuerpo}
+                    />)
+            )}
         </section>
     );
     }
